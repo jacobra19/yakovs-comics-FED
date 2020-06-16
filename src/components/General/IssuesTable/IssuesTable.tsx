7895b015eb008642afaa9394d6b84640d7ff2317
Yakov Rakhamimov <jacobra19@gmail.com>
2020-06-17 01:39:39 +0300

    IssuesTable: Styles

--- src/components/General/IssuesTable/IssuesTable.tsx
+++ src/components/General/IssuesTable/IssuesTable.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Typography, Checkbox } from '@material-ui/core';
-import { ComicBook } from "../../../types";
+import { ComicBook,Styles } from "../../../types";
 import { isEmpty as _isEmpty } from "lodash";
 
 interface IssuesTableProps {
@@ -11,12 +11,40 @@ interface IssuesTableProps {
 const IssuesTable: React.FC<IssuesTableProps> = (props) => {
     if(_isEmpty(props.issues)) return null
 
+    const styles = (s:string,isChecked?:boolean) => {
+        let styles:Styles = {
+            root: {
+
+            },
+            issueRow:{
+                height: 120, 
+                display:'flex',
+                alignItems: "center",
+                marginBottom:10,
+            },
+            checkBox:{
+                width:24,
+                height:24
+            },
+            issueCover:{
+                width:80,
+                height: "100%",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                margin:'0px 10px 0px 5px'
+            },
+            issueTitle:{
+                backgroundColor: isChecked ? '#f50057' : 'initial',
+                color: isChecked ? 'white' : 'initial',
+            }
+        }
+
+        return(styles[s]);
+    }
+
     const renderCheckBox = (item:ComicBook,idx:number) => {
         return (
-            <Checkbox   style={{
-                            width:24,
-                            height:24
-                        }}
+            <Checkbox   style={styles('checkBox',item.isChecked)}
                         checked={item.isChecked}
                         onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{props.onCheckboxChange(e,idx)}}
                         inputProps={{ 'aria-label': 'secondary checkbox' }}
@@ -24,53 +52,41 @@ const IssuesTable: React.FC<IssuesTableProps> = (props) => {
         )
     }
 
-    const renderIssueCover = (coverSrc: string) => {
+    const renderIssueCover = (item:ComicBook) => {
 
         return(
             <div style={{
-                    backgroundImage: `url(${coverSrc})`,
-                    width:80,
-                    height: "100%",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    margin:'0px 10px 0px 5px'
-                }}>
-            </div>
+                ...styles('issueCover'), 
+                backgroundImage: `url(${item.media.coverSrc})`
+            }}></div>
         )
     }
 
     const renderIssueTitle = (item:ComicBook) => {
 
         return(
-            <Typography style={{
-                            backgroundColor: item.isChecked ? '#f50057' : 'initial',
-                            color: item.isChecked ? 'white' : 'initial',
-                        }}
+            <Typography style={styles('issueTitle')}
             >
                 {item.series.title} {item.series.issue}
             </Typography>
         )
     }
 
+    const renderIssueRow = (item:ComicBook,idx:number) => {
+
+        return(
+            <div key={idx} style={styles('issueRow')}>
+                { renderCheckBox(item,idx) }
+                { renderIssueCover(item) }
+                { renderIssueTitle(item) }
+            </div>
+        )
+    }
+
 
     return (
-        <div>
-            {
-                props.issues.map((item,idx)=>{
-                    return (
-                        <div key={idx} style={{
-                            height: 120, 
-                            display:'flex',
-                            alignItems: "center",
-                            marginBottom:10,
-                        }}>
-                            { renderCheckBox(item,idx) }
-                            { renderIssueCover(item.media.coverSrc) }
-                            { renderIssueTitle(item) }
-                        </div>
-                    )
-                })
-            }
+        <div style={styles('root')}>
+            { props.issues.map(renderIssueRow) }
         </div>
     )
 }
