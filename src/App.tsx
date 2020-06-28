99ee00bd0a462b874641b818abdfba9b661060d8
Yakov Rakhamimov <jacobra19@gmail.com>
2020-06-29 02:11:11 +0300

    getAccessTokenSilently

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from 'recoil';
+require('dotenv').config()
 
 import TopBar from './components/Layout/TopBar/TopBar'
 import MainSection from './components/Layout/MainSection/MainSection'
+
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 import _ from "lodash";
 import moment from 'moment';
 
 declare global {
-    interface Window { 
-        _: any, 
-        moment: any, 
+    interface Window {
+        _: any,
+        moment: any,
     }
 }
 
@@ -22,16 +26,20 @@ window.moment = moment || {};
 
 function App() {
     interface Styles {
-        [key:string]: object
+        [key: string]: object
     }
-    const styles = (s:string): object => {
-        let styles:Styles = {
+
+    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const [userMetadata, setUserMetadata] = useState(null);
+
+    const styles = (s: string): object => {
+        let styles: Styles = {
             root: {
                 width: "100vw",
                 height: "100vh",
                 overflow: "auto",
             },
-            mainSection:{
+            mainSection: {
 
             },
         }
@@ -39,15 +47,49 @@ function App() {
         return styles[s];
     }
 
-    
+    useEffect(() => {
+        const getUserMetadata = async () => {
+            const domain = process.env.REACT_APP_Auth0_Domain;
+            console.log('domain', domain)
+            try {
+                const accessToken = await getAccessTokenSilently({
+                    audience: `https://${domain}/api/v2`,
+                    scope: "read:current_user",
+                });
 
-    
+                const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+                const metadataResponse = await fetch(userDetailsByIdUrl, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                });
+
+                const { user_metadata } = await metadataResponse.json();
+
+                setUserMetadata(user_metadata);
+            } catch (e) {
+                console.log(e.message);
+            }
+        };
+
+        getUserMetadata();
+    }, []);
+
+    useEffect(() => {
+        console.log('userMetadata', userMetadata)
+        return () => {
+            // cleanup
+        }
+    }, [userMetadata])
+
+
     return (
         <RecoilRoot>
             <Router>
                 <div className="App" style={styles('root')}>
-                    <TopBar/>
-                    <MainSection/>
+                    <TopBar />
+                    <MainSection />
                 </div>
             </Router>
         </RecoilRoot>
