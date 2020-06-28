2b1350aac682b22dd1c9b498b61bac5436d6e9d9
Yakov Rakhamimov <jacobra19@gmail.com>
2020-06-28 18:06:03 +0300

    ag-grid

--- src/views/Table/Table.tsx
+++ src/views/Table/Table.tsx
@@ -1,15 +1,31 @@
 import React, { useState } from 'react';
-import getComicsCollection from "../../actions/getComicsCollection";
-import addFromScrapeComics from '../../actions/addFromScrapeComics'
 
 import { Typography, TextField, Button } from '@material-ui/core';
 import { isEmpty as _isEmpty } from 'lodash';
 
 import { ComicBook, Styles } from "../../types";
 
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
 
 const Table = () => {
+    const columnDefs = [{
+        headerName: "Make", field: "make", sortable: true
+      }, {
+        headerName: "Model", field: "model"
+      }, {
+        headerName: "Price", field: "price"
+      }]
 
+      const rowData= [{
+        make: "Toyota", model: "Celica", price: 35000
+      }, {
+        make: "Ford", model: "Mondeo", price: 32000
+      }, {
+        make: "Porsche", model: "Boxter", price: 72000
+      }]
     const styles = (s:string) => {
         let styles:Styles = {
             root: {
@@ -28,8 +44,12 @@ const Table = () => {
 
 
     return (
-        <div style={styles('root')}>
-            table here
+        <div style={styles('root')}         className="ag-theme-alpine"
+        >
+            <AgGridReact
+            columnDefs={columnDefs}
+            rowData={rowData}>
+            </AgGridReact>
         </div>
     )
 }
